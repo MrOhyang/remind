d39116abf9187f19cef79d02c3f960bfe0fe8f34
Ouyang Yibin <ohyang94e@163.com>
2017-12-13 23:03:50 +0800

    update 日期选择组件

--- src/components/dateSelector.vue
+++ src/components/dateSelector.vue
@@ -1,5 +1,10 @@
 <template>
 <div class="date-selector-wrapper">
+
+  <div class="row month-wrapper">
+    十二月
+  </div>
+
   <!-- 星期 -->
   <div class="row week-title-wrapper">
     <span>日</span>
@@ -17,10 +22,12 @@
         <span :key="i"></span>
       </template>
       <template v-else>
-        <span :key="i">{{ day.num }}</span>
+        <span :class="{active: day.active}" :key="i"
+              @click="clickDay(day)">{{ day.num }}</span>
       </template>
     </template>
   </div>
+
 </div>
 </template>
 
@@ -29,17 +36,27 @@ import moment from 'moment'
 export default {
   data() {
     return {
-      date_list: []
+      current_year: null,  // 当前年
+      current_month: null,  // 当前月
+
+      selected_day: null,  // 选中的天
+      selected_day_data: null,  // 选中的天的数据
+
+      date_list: [],
     }
   },
   created() {
-    this.mockData();
+    this.initDay();
   },
   methods: {
     // 模拟数据
-    mockData() {
+    initDay() {
       let first_day = moment();
       let num_max = 0;
+
+      this.current_year = first_day.year();
+      this.current_month = first_day.month();
+      this.selected_day = moment();
 
       first_day.add(1 - first_day.date(), 'd');
       for (let i = 0, len = first_day.day(); i < len; i++) {
@@ -47,12 +64,30 @@ export default {
           is_empty: true
         });
       }
-      num_max = first_day.add(1, 'M').add(-1, 'd').date();
-      for (let i = 1, len = num_max; i <= len; i++) {
-        this.date_list.push({
-          num: i
-        });
+      while(first_day.month() == this.current_month) {
+        let temp = {
+          num: first_day.date(),
+          time: moment(first_day)
+        };
+        if (first_day.month() == this.selected_day.month()
+            && first_day.date() == this.selected_day.date()) {
+          temp.active = true;
+          this.selected_day_data = temp;
+        }
+        this.date_list.push(temp);
+        first_day.add(1, 'd');
       }
+    },
+    // 选中当天
+    clickDay(day) {
+      console.log(day);
+      if (this.selected_day.format('YYYY-MM-DD') == day.time.format('YYYY-MM-DD')) {
+        return ;
+      }
+      this.selected_day_data.active = false;
+      this.$set(day, 'active', true);
+      this.selected_day = moment(day.time);
+      this.selected_day_data = day;
     }
   }
 }
