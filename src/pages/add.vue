ae0fcc3d2302f0377b652cdd2d6eb7178ac4a9ec
Ouyang Yibin <ohyang94e@163.com>
2017-12-14 14:14:49 +0800

    update 添加提醒交互

--- src/pages/add.vue
+++ src/pages/add.vue
@@ -4,7 +4,7 @@
   <!-- 标题 -->
   <div class="add-title-wrapper">
     <router-link to="/list" class="iconfont icon-arrow-left"></router-link>
-    <h1>添加提醒</h1>
+    <h1>添加提醒{{ this.time }}</h1>
   </div>
 
   <div class="main-input-wrapper">
@@ -19,7 +19,9 @@
   <!-- 设置时间按钮 -->
   <div class="list-wrapper">
     设置提醒时间：
-    <input type="time" class="input-time">
+    <input type="time" class="input-time" step="300"
+           v-model="time"
+           @input="transferInputTime()">
   </div>
 
   <button class="btn-save" @click="addTask()">保存</button>
@@ -34,28 +36,82 @@ import dateSelector from '@/components/dateSelector.vue';
 export default {
   data() {
     return {
-      content: ''
+      content: '',  // 用户输入要提醒是事
+      time: null,   // 用户输入要提醒的时间
+
+      is_sending: false,  // 是否正在发送
     };
   },
   components: {
     dateSelector
   },
   methods: {
+    // 初始化数据
+    initData() {
+      this.content = '';
+    },
     // 添加提醒事件
     addTask() {
-      let phone = this.$store.state.user.phone,
-          time = null;
+      if (this.is_sending) return ;
 
-      time = moment(this.$refs.date_selector.selected_day);
+      let phone = this.$store.state.user.phone,
+          ymd = this.$refs.date_selector.selected_day.format('YYYY-MM-DD'),
+          time = `${ymd} ${this.time}:00`;
+
+      // 校验
+      if (this.content.length <= 0) {
+        this.$message({
+          showClose: true,
+          message: '您未输入要提醒的事',
+          type: 'error'
+        });
+        return ;
+      }
+      if (this.time == null) {
+        this.$message({
+          showClose: true,
+          message: '请输入提醒时间',
+          type: 'error'
+        });
+        return ;
+      }
+
+      time = moment(time).unix();
 
       // console.log(phone, time, this.content);
-      return ;
+      this.is_sending = true;
       this.$http.post('/api/ReMind/Task/addTask', qs.stringify({
         phone_str: phone,
-        send_time: '',
+        send_time: time,
         send_content: this.content
-      }));
-    }
+      })).then(r => {
+        this.is_sending = false;
+        if (r.code == 0) {
+          this.$message({
+            showClose: true,
+            message: '添加成功',
+            type: 'success'
+          });
+          this.initData();
+        } else {
+          this.$message({
+            showClose: true,
+            message: r.msg,
+            type: 'error'
+          });
+        }
+      }).catch(e => {
+        this.is_sending = false;
+        this.$message({
+          showClose: true,
+          message: '发生错误',
+          type: 'error'
+        });
+      });
+    },
+    // 控制input在5min倍数
+    transferInputTime() {
+    },
   }
 }
 </script>
